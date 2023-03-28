7391a0f6105784c292c27ccbdde0da7c8ea494c0
Arno luyckx <A.luyckx@protonmail.com>
2023-03-28 15:46:49 +0200

    React-Router-Dom installed, made base components
    Co-authored-by: Emma Lou Who <Emmah-lou@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,93 @@
 import { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
-// import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom"; // ? npm install react-router-dom - May be of use later
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom"; // ? npm install react-router-dom - May be of use later
 import "./App.css";
+
+const Home = () => {
+  return (
+    <>
+      /* Header component */
+      <header>
+        {" "}
+        {/* //? Banner for background img */}
+        <h1>Iris Arts</h1>
+        <h2>""In every drawing there's a story""</h2>
+      </header>
+    </>
+  );
+};
+
+const About = () => {
+  return (
+    <>
+      /* Header component */
+      <header>
+        {" "}
+        {/* //? Banner for background img */}
+        <h1>About</h1>
+        <h2>""In every drawing there's a story""</h2>
+      </header>
+    </>
+  );
+};
+
+const Contact = () => {
+  return (
+    <>
+      /* Header component */
+      <header>
+        {" "}
+        {/* //? Banner for background img */}
+        <h1>Contact</h1>
+        <h2>""In every drawing there's a story""</h2>
+      </header>
+    </>
+  );
+};
+
+const Gallery = () => {
+  return (
+    <>
+      /* Header component */
+      <header>
+        {" "}
+        {/* //? Banner for background img */}
+        <h1>Gallery</h1>
+        <h2>""In every drawing there's a story""</h2>
+      </header>
+    </>
+  );
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Iris Arts</h1>
-      </header>
-      <main>
-        <h2>hero</h2>
-      </main>
-      <footer>
-        <h3>footer</h3>
-      </footer>
+      <Router>
+        <Switch>
+          <Route path="/" exact component={Home}>
+            <Home />
+          </Route>
+          <Route path="/about" exact component={About}>
+            <About />
+          </Route>
+          <Route path="/contact" exact component={Contact}>
+            <Contact />
+          </Route>
+          <Route path="/gallery" exact component={Gallery}>
+            <Gallery />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
 
 export default App;
+
+/**
+ * ? Going for bootstrap layout and margin/paddings?
+ * ? https://getbootstrap.com/docs/5.0/layout/overview/
+ *
+ * ? carousel... Bootstrap 5 carousel tis time?
+ */
