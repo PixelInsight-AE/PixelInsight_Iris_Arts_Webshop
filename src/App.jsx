df3a76c7312de511df9352681194511ab45b1039
Emma Lou Who <122497149+Emmah-lou@users.noreply.github.com>
2023-03-30 06:33:27 -0600

    fixed the styling for the about me page, also fixed color of the comming soon card to be consistent with the other pages.

--- src/App.jsx
+++ src/App.jsx
@@ -72,8 +72,8 @@ function App() {
       <Route path="/gallery/" component={Gallery} />
       <Route path="/shop/" component={Shop} />
 
-      <Footer />
       {/* //! Here goes footer component which also stays the same across all tabs */}
+      <Footer />
     </Router>
   );
 }
