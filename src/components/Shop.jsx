16debf621310f281127bf80999a8db4b68d4e858
Arno luyckx <A.luyckx@protonmail.com>
2023-04-13 11:20:13 +0200

    styling..
    Co-authored-by: Emma Lou Who <Emmah-lou@users.noreply.github.com>

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -2,28 +2,30 @@ const Shop = () => {
   return (
     <>
       <main>
-        <div className="shop-card">
-          <h1>COMING SOON!</h1>
-          <hr />
-          <div className="d-flex d-flex-row">
-            <div className="coming-soon">
-              <p className="dutch-message">
-                ("Er word nog gewerkt aan de webwinkel.
-                <br />
-                Kom binnenkort gerust weer een kijkje nemen!")
-              </p>
-              <hr />
-              <p className="english-message">
-                ("We are currently working on our shop.
-                <br /> Please check back soon for more information!")
-              </p>
-            </div>
-            <div style={{ marginLeft: 100 }}>
-              <img
-                className="img-fluid "
-                src="https://i.imgur.com/W6LNlGE.png"
-                alt=""
-              />
+        <div className="shop-container">
+          <div className="shop-card">
+            <h1>COMING SOON!</h1>
+            <hr />
+            <div className="d-flex d-flex-row">
+              <div className="coming-soon">
+                <p className="dutch-message">
+                  ("Er word nog gewerkt aan de webwinkel.
+                  <br />
+                  Kom binnenkort gerust weer een kijkje nemen!")
+                </p>
+                <hr />
+                <p className="english-message">
+                  ("We are currently working on our shop.
+                  <br /> Please check back soon for more information!")
+                </p>
+              </div>
+              <div style={{ marginLeft: 100 }}>
+                <img
+                  className="img-fluid "
+                  src="https://i.imgur.com/W6LNlGE.png"
+                  alt=""
+                />
+              </div>
             </div>
           </div>
         </div>
